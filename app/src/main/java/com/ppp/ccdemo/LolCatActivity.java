a3bad5ccf4a133f475e8907db8c3fbd26f8151fa
Stan Dolson <sdolson@ebay.com>
2015-10-02 21:31:12 -0700

    Updated app icon and used for the progress spinner.  Updated the layouts to     include a progress spinner.

--- app/src/main/java/com/ppp/ccdemo/LolCatActivity.java
+++ app/src/main/java/com/ppp/ccdemo/LolCatActivity.java
@@ -2,15 +2,53 @@ package com.ppp.ccdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class LolCatActivity extends AppCompatActivity
 {
 
+	private ImageView lolcatImage;
+	private TextView lolcatTitle;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_lol_cat);
+
+		lolcatTitle = (TextView)findViewById(R.id.lolcat_title);
+
+		lolcatImage = (ImageView)findViewById(R.id.lolcat_image);
+		lolcatImage.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v)
+			{
+				lolcatImage.setClickable(false);
+				showProgress();
+			}
+		});
+
+		hideProgress();
 	}
 
+	/**
+	 * Shows the cat progress spinner
+ 	 */
+	private void showProgress()
+	{
+		findViewById(R.id.lolcat_progress).setVisibility(View.VISIBLE);
+		lolcatTitle.setText(R.string.lolcat_waiting_title);
+	}
+
+	/**
+	 * Hides the cat progress spinner
+	 */
+	private void hideProgress()
+	{
+		findViewById(R.id.lolcat_progress).setVisibility(View.GONE);
+	}
 }
